a1a903da2e184e42b58fecfc9ba735f9ff30a862
Martins <victorcostamartins0@gmail.com>
2025-08-01 12:08:51 -0300

    update teste

--- tests/Feature/LivroControllerTest.php
+++ tests/Feature/LivroControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Models\Livro;
+use App\Models\User;
 use Database\Factories\LivroFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -13,29 +14,46 @@ class LivroControllerTest extends TestCase
 
     public function test_create_livro()
     {
-        $livro = LivroFactory::new()->make()->toArray();
+        $livro = Livro::factory()->make()->toArray();
 
         $response = $this->post('/livro/store', $livro);
 
         $response->assertRedirect();
 
-        // $this->assertDatabaseHas('livros', [
-        //     'titulo' => $livro['titulo']
-        // ]);
+        $this->assertDatabaseHas('livros', [
+            'titulo' => $livro['titulo']
+        ]);
     }
     
     public function test_update_livro()
     {
         $livro = Livro::factory()->create();
 
-        $novosDados = LivroFactory::new()->make()->toArray();
+        $novosDados = Livro::factory()->make()->toArray();
 
         $responde = $this->put("/livro/{$livro->id}", $novosDados);
 
         $responde->assertRedirect();
 
         $this->assertDatabaseHas('livros', [
-            'titulo' => $novosDados
+            'titulo' => $novosDados['titulo']
+        ]);
+    }
+
+    public function test_destroy_livro()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $livro = Livro::factory()->create();
+
+        $responde = $this->delete("/livro/{$livro->id}");
+
+        $responde->assertRedirect();
+
+        $this->assertDatabaseMissing('livros', [
+            'id' => $livro->id
         ]);
     }
 }
